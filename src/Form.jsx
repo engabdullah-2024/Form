e9bbae9da6fee64e7126bf804001832cbefcf34c
engabdullah-2024 <enga95311@gmail.com>
2024-07-28 13:29:51 +0300

    Updated

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,20 +1,21 @@
 function Form({close}){
-    return<div className="bg-black h-screen absolute w-full top-0  bg-opacity-50">
+    return<div className=" h-screen absolute w-full top-0 bg-opacity-5 bg-black  ">
 
   
     
-    <div className="flex justify-center pt-20">
+    <div className="flex justify-center sm:pt-20 pt-10 mt-20">
 
-<form className="bg-white w-[400px] p-3 h-[400px]">
-<i  onClick={close}   class="fa-solid fa-x  mr-[300px] text-3xl "></i>
+<form className="bg-blue-500 sm:w-[400px]sm:p-3 sm:h-[400px] w-[300px] h-[350px] rounded mr-20">
+<i  onClick={close}   class="fa-solid fa-x  mr-[350px] text-3xl  "></i>
+<h1 className="text-3xl font-bold">Fill This Form</h1>
 
 
 <br/>
-   <input className="w-[350px] my-3 h-[40px] border-2 border-black "       type="text" placeholder="Enter Title"></input>
+   <input className="sm:w-[350px]sm:my-3 sm:h-[40px] border-2 border-black  w-[250px] h-[30px]"       type="text" placeholder="Enter Title"></input>
    <br/>
-   <textarea className="border-2 border-black"    rows={8} cols={46} />
+   <textarea className="border-2 border-black mt-6"    rows={4} cols={40} />
    <br/>
-   <button className="bg-black text-white px-[150px] my-5 py-2 text-3xl rounded">Save</button>
+   <button className="bg-black text-white sm:px-[150px] sm:my-5 sm:py-2 text-3xl rounded px-[120px] my-3 py-1">Save</button>
 </form>
     </div>
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -16,10 +16,10 @@ function Header(){
     
     
     
-    <div className="flex justify-between bg-purple-500 px-10 py-4 items-center">
+    <div className="flex justify-between bg-purple-500 px-3 py-3    sm:px-10 sm:py-4 items-center">
         <h1 className="text-4xl text-white">Nooty</h1>
        
-        <button onClick={handleOpen} className="bg-orange-500 px-10 py-4 hover:bg-white hover:text-orange-500  ">Add note</button>
+        <button onClick={handleOpen} className="bg-orange-500 px-5 py-2 sm:px-10 sm:py-4 hover:bg-white hover:text-orange-500    ">Add note</button>
     </div>
     {
         isOpen == true ? <Form  close={handleClose}  /> : ""
